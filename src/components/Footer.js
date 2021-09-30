e3027851d282128c1f1efc95b057808516a979a8
afleissner2019 <af_ajay@yahoo.com>
2021-09-30 02:58:31 +0200

    review the codes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,24 @@ function Footer(props) {
   return (
     <div class="icons">
       <p class="company-name">ABC &copy; 2021, ALL Rights Reserved</p>
-      <a href="https://www.facebook.com/" target="_blank">
+      <a
+        href="https://www.facebook.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i class="fa fa-facebook fa-1x" target="_blank"></i>
       </a>
-      <a href="https://www.linkedin.com/" target="_blank">
-        <i class="fa fa-linkedin fa-1x" target="_blank"></i>
+      <a
+        href="https://www.linkedin.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fa fa-linkedin fa-1x"></i>
       </a>
       <a href="https://www.instagram.com/">
-        <i class="fa fa-instagram fa-1x" target="_blank"></i>
+        <i class="fa fa-instagram fa-1x"></i>
       </a>
-      <a href="https://twitter.com/" target="_blank">
+      <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
         <i class="fa fa-twitter fa-1x"></i>
       </a>
     </div>
